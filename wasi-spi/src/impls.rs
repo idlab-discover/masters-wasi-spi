0eea82df2ea33f1f5d4979b8ad6a3aad2bec128a
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2025-12-13 18:28:31 +0100

    better transfer

--- wasi-spi/src/impls.rs
+++ wasi-spi/src/impls.rs
@@ -22,6 +22,49 @@ impl<'a, T: WasiSpiView> SpiImpl<'a, T> {
     }
 }
 
+fn to_spi_err(e: impl ToString) -> spi_bindings::Error {
+    spi_bindings::Error::Other(e.to_string())
+}
+
+enum TransactionBuffer {
+    Read(Vec<u8>),
+    Write(Vec<u8>),
+    Transfer { read: Vec<u8>, write: Vec<u8> },
+    Delay(u32),
+}
+
+impl TransactionBuffer {
+    fn from_op(op: spi_bindings::Operation) -> Self {
+        match op {
+            spi_bindings::Operation::Read(len) => Self::Read(vec![0; len as usize]),
+            spi_bindings::Operation::Write(data) => Self::Write(data),
+            spi_bindings::Operation::Transfer(data) => Self::Transfer {
+                read: vec![0; data.len()],
+                write: data,
+            },
+            spi_bindings::Operation::DelayNs(ns) => Self::Delay(ns),
+        }
+    }
+
+    fn as_hal_op(&mut self) -> HalOperation<'_, u8> {
+        match self {
+            Self::Read(buf) => HalOperation::Read(buf),
+            Self::Write(buf) => HalOperation::Write(buf),
+            Self::Transfer { read, write } => HalOperation::Transfer(read, write),
+            Self::Delay(ns) => HalOperation::DelayNs(*ns),
+        }
+    }
+
+    fn into_result(self) -> spi_bindings::OperationResult {
+        match self {
+            Self::Read(buf) => spi_bindings::OperationResult::Read(buf),
+            Self::Write(_) => spi_bindings::OperationResult::Write, // In a write, the Guest should not be returned anything
+            Self::Transfer { read, .. } => spi_bindings::OperationResult::Transfer(read),
+            Self::Delay(_) => spi_bindings::OperationResult::Delay,
+        }
+    }
+}
+
 impl<'a, T: WasiSpiView> spi_bindings::Host for SpiImpl<'a, T> {
     fn get_device_names(&mut self) -> Vec<String> {
         self.ctx().devices.keys().cloned().collect()
@@ -143,60 +186,17 @@ impl<'a, T: WasiSpiView> spi_bindings::HostSpiDevice for SpiImpl<'a, T> {
         handle: Resource<ActiveSpiDriver>,
         operations: Vec<spi_bindings::Operation>,
     ) -> Result<Vec<spi_bindings::OperationResult>, spi_bindings::Error> {
-        let spi = self
-            .table()
-            .get_mut(&handle)
-            .map_err(|e| spi_bindings::Error::Other(e.to_string()))?;
-
-        enum TransactionBuffers {
-            Read(Vec<u8>),
-            Write(Vec<u8>),
-            Transfer { read: Vec<u8>, write: Vec<u8> },
-            DelayNs(u32),
-        }
-
-        let mut buffers: Vec<TransactionBuffers> = operations
+        let spi = self.table().get_mut(&handle).map_err(to_spi_err)?;
+        let mut buffers: Vec<_> = operations
             .into_iter()
-            .map(|operation| match operation {
-                spi_bindings::Operation::Read(len) => {
-                    TransactionBuffers::Read(vec![0u8; len as usize])
-                }
-                spi_bindings::Operation::Write(write_buf) => TransactionBuffers::Write(write_buf),
-                spi_bindings::Operation::Transfer(write_buf) => TransactionBuffers::Transfer {
-                    read: vec![0u8; write_buf.len()],
-                    write: write_buf,
-                },
-                spi_bindings::Operation::DelayNs(ns) => TransactionBuffers::DelayNs(ns),
-            })
+            .map(TransactionBuffer::from_op)
             .collect();
-
-        let mut hal_operations: Vec<HalOperation<u8>> = buffers
-            .iter_mut()
-            .map(|buffer| match buffer {
-                TransactionBuffers::Read(buf) => HalOperation::Read(buf),
-                TransactionBuffers::Write(buf) => HalOperation::Write(buf),
-                TransactionBuffers::Transfer { read, write } => HalOperation::Transfer(read, write),
-                TransactionBuffers::DelayNs(ns) => HalOperation::DelayNs(*ns),
-            })
-            .collect();
-
-        spi.device
-            .transaction(&mut hal_operations)
-            .map_err(|e| spi_bindings::Error::Other(e.to_string()))?;
-
-        let results = buffers
+        let mut hal_ops: Vec<_> = buffers.iter_mut().map(|b| b.as_hal_op()).collect();
+        spi.device.transaction(&mut hal_ops).map_err(to_spi_err)?;
+        Ok(buffers
             .into_iter()
-            .map(|buffer| match buffer {
-                TransactionBuffers::Read(data) => spi_bindings::OperationResult::Read(data),
-                TransactionBuffers::Write(_) => spi_bindings::OperationResult::Write,
-                TransactionBuffers::Transfer { read, .. } => {
-                    spi_bindings::OperationResult::Transfer(read)
-                }
-                TransactionBuffers::DelayNs(_) => spi_bindings::OperationResult::Delay,
-            })
-            .collect();
-
-        Ok(results)
+            .map(TransactionBuffer::into_result)
+            .collect())
     }
 
     fn drop(&mut self, rep: Resource<ActiveSpiDriver>) -> wasmtime::Result<()> {
