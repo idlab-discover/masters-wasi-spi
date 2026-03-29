91dc6daca83e844c2857954a8d0661e23e40e310
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-03-29 16:34:00 +0200

    add benchmarking python

--- flake.nix
+++ flake.nix
@@ -7,43 +7,55 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ rust-overlay.overlays.default ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-          config.allowUnfree = true;
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [rust-overlay.overlays.default];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+        config.allowUnfree = true;
+      };
 
-        # Pico 2 uses Arm Cortex-M33 (thumbv8m)
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          targets = [
-            "thumbv8m.main-none-eabihf"
-            "wasm32-unknown-unknown"
-            "wasm32-wasip1"
-            "wasm32-wasip2"
-          ];
-          extensions = [ "rust-src" "clippy" "llvm-tools-preview" ];
-        };
+      # Pico 2 uses Arm Cortex-M33 (thumbv8m)
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        targets = [
+          "thumbv8m.main-none-eabihf"
+          "wasm32-unknown-unknown"
+          "wasm32-wasip1"
+          "wasm32-wasip2"
+        ];
+        extensions = ["rust-src" "clippy" "llvm-tools-preview"];
+      };
 
-      in {
-        devShells.default = pkgs.mkShell {
-          name = "pico2-shell";
+      pythonEnv = pkgs.python3.withPackages (ps:
+        with ps; [
+          pandas
+          numpy
+          matplotlib
+          seaborn
+        ]);
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "pico2-shell";
 
-          packages = [
-            rustToolchain
-            pkgs.elf2uf2-rs # Flashing tool
-            pkgs.picotool # Official Pico utility (optional but good)
-            pkgs.flip-link # Linker optimizer (standard in embedded rust)
-            pkgs.cargo-generate
-            pkgs.cargo-component
-            pkgs.wasmtime
-            pkgs.wac-cli
-            pkgs.wasm-tools
-            pkgs.wabt
-            pkgs.probe-rs-tools
-          ];
-        };
-      });
+        packages = [
+          rustToolchain
+          pkgs.elf2uf2-rs # Flashing tool
+          pkgs.picotool # Official Pico utility (optional but good)
+          pkgs.flip-link # Linker optimizer (standard in embedded rust)
+          pkgs.cargo-generate
+          pkgs.cargo-component
+          pkgs.wasmtime
+          pkgs.wac-cli
+          pkgs.wasm-tools
+          pkgs.wabt
+          pkgs.probe-rs-tools
+          pythonEnv
+        ];
+      };
+    });
 }
